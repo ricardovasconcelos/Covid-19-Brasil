5e0a65c007a5eaa8d6d3bb17701dfc25eb9bb88d
ricardovasconcelos <ricardovasconcelos.dev@gmail.com>
2020-04-17 09:48:02 -0300

    Updated endpoint api

--- src/service/data.js
+++ src/service/data.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const data = axios.create({
-  baseURL: "https://corona.lmao.ninja/countries/brazil"
+  baseURL: "https://corona.lmao.ninja/v2/countries/brazil"
 });
 
 export default data;
